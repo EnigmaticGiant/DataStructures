6d71d30f6aec0b849deabbec53beb49b7e9538b6
Titus Davis <tidus.tavis@gmail.com>
2020-02-17 00:57:58 -0600

    Add comparator files to GST

---
+++ GST/comparator.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#ifndef _COMP_INCLUDED_
+#define _COMP_INCLUDED_
+
+
+//extern int intComparator(void*, void*);
+extern int realComparator(void *x, void *y);
+extern int stringComparator(void *x, void *y);
+
+#endif
